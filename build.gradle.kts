48f422e6ede6210430308536bef4a726bd576952
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2023-04-15 12:10:39 +0200

    Change dependencies order

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,17 @@ val kotestVersion = "5.5.5"
 dependencies {
     implementation("io.vavr:vavr:0.10.4")
 
+    testImplementation("org.apache.commons:commons-lang3:3.12.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.awaitility:awaitility:4.2.0")
 
     testImplementation("io.kotest:kotest-framework-api:$kotestVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.mockk:mockk:1.13.5")
-    testImplementation("org.apache.commons:commons-lang3:3.12.0")
-    testImplementation("org.awaitility:awaitility:4.2.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 
     testImplementation("org.spockframework:spock-core:2.3-groovy-3.0")
